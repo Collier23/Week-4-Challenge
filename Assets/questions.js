dc6290d09ab8c1d977cc86a707a90cc717cd5a00
Collier23 <ryancollier1994@gmail.com>
2022-09-11 10:21:19 +1000

    made changes to js and added question js

---
+++ Assets/questions.js
@@ -0,0 +1,22 @@
+let questions = [
+    {title: "Commonly used data types DO NOT include:",
+     choices: ["alerts", "quotes", "curly brackets", "parentheses"], 
+     answer: "alerts"},
+
+    {title: "The condition in an if / else statement is enclosed within ____.",
+     choices: ["quotes", "parentheses", "curly brackets", "commas"],
+     answer: "parentheses"},
+
+    {title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"},
+
+    {title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"},
+
+    {title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["console.log", "terminal/bash", "for loops", "JavaScript"],
+    answer: "console.log"}
+];
+
